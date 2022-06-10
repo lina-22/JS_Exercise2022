21eae7d85c96a878624cff3e68c7255e34ed33c7
lina-22 <haquefahinur@gmail.com>
2022-06-10 21:26:54 +0200

    js parcticing

---
+++ jsObj/test.js
@@ -0,0 +1,29 @@
+const car = {
+    names: "Fiat",
+    model:500,
+    weight: "850kg",
+    color : "white",
+    strat: function (){
+        console.log("car has started");
+    },
+    drive: function () {
+        console.log("car is driving");
+    },
+
+ };
+//  console.log(car.weight);
+
+ console.log(car.names);
+
+
+ const car1 = {
+     
+     names1: "Toyata",
+     model:"500",
+     start: function(){
+         console.log("car has started");
+     }
+
+ }
+
+ console.log(car1.model);
